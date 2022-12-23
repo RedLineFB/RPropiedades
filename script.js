a1322f903001d346edc544e350aaf49755f809e7
RedLineFB <betancurfacundo@gmail.com>
2022-12-23 12:14:44 -0300

    PP2

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("fotos1").addEventListener("click",e=>{
+		window.location.href = "fotos Red.html"
+	})
+
+document.getElementById("fotos2").addEventListener("click",e=>{
+		window.location.href = "Fotos Green and yellow.html"
+	})
+
+document.getElementById("fotos.comod").addEventListener("click",e=>{
+		window.location.href = "comodidades.html"
+	})
+ 
